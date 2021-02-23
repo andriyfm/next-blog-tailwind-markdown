176128759d26cd04985404ea99140a02b04fd201
andriyfm <andrifmsyh@gmail.com>
2021-02-23 13:02:45 +0700

    Setup typescript

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Footer from "../components/Footer";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>My Blog</h1>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
