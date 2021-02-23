0d034fb1b6bae1f673c55d4d87a010210a219c0d
andriyfm <andrifmsyh@gmail.com>
2021-02-23 13:09:31 +0700

    Create about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,17 +1,17 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
-import styles from "../styles/Home.module.css";
+import Footer from "../../components/Footer";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home</title>
+        <title>About</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Home</h1>
+        <h1 className={styles.title}>About</h1>
       </main>
 
       <Footer />
